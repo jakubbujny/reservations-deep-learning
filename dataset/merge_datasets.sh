869590965d86d184d760017d7b8b19ba4a596083
Jakub Bujny <jakub.bujny@ac-project.net>
2017-12-28 18:39:54 +0100

    upgrade scripts for merging

--- dataset/merge_datasets.sh
+++ dataset/merge_datasets.sh
@@ -1,3 +1,11 @@
+# https://stackoverflow.com/questions/5143795/how-can-i-check-in-a-bash-script-if-my-local-git-repo-has-changes
+if [[ `git status --porcelain` ]]; then
+	  # changes
+	  echo "Changes detected, rebuilding datasets"
+else
+	exit 0
+fi
+
 ruby data_set_transformation.rb dataset1.csv
 ruby data_set_transformation.rb dataset2.csv
 ruby data_set_transformation.rb dataset3.csv
